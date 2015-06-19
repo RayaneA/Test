503c1280c2278f23efed76cfdbabcfa873f6e64a
RayaneA <rayane.alami95@gmail.com>
2015-06-19 15:54:35 +0200

    app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
-  res.send('Hello biatch!');
+  res.send('Bonjour humain!');
 });
 
 var server = app.listen(3000, function () {
